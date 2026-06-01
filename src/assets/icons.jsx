e4f813090e65121b4663ded722ba321c8039a481
djibril424 <141808900+djibril424@users.noreply.github.com>
2026-06-01 21:24:50 +0100

    Import and export icons from lucide-react

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,1 +1,26 @@
+import {
+  Home,
+  MessageCircle as MessageSquare,
+  User,
+  PlusCircle,
+  Users,
+  Settings,
+  ArrowLeft,
+  Trash2,
+  Search,
+  Send
+} from 'lucide-react';
+
+export {
+  Home,
+  MessageSquare,
+  User,
+  PlusCircle,
+  Users,
+  Settings,
+  ArrowLeft,
+  Trash2,
+  Search,
+  Send
+};
 
