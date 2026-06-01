5640ffc597eb7731158f916fb915f0f670cd1f63
djibril424 <141808900+djibril424@users.noreply.github.com>
2026-06-01 23:09:43 +0100

    Initialize Supabase client in supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,12 @@
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://yetqpyuknmwlwooeczag.supabase.co';
+
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldHFweXVrbm13bHdvb2VjemFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjE3NzEsImV4cCI6MjA5NTg5Nzc3MX0.ms-cNVZ2TQcpmVSUtFfiA36mG5REEaCbTtQSZV6j0mo';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
