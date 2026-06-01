1f668d80b5fd96b0a3d4abd63a70bff8f1cc5922
djibril424 <141808900+djibril424@users.noreply.github.com>
2026-06-01 20:54:24 +0100

    Add MessageItem component for displaying messages

--- src/ui/message.jsx
+++ src/ui/message.jsx
@@ -1,1 +1,14 @@
 
+import React from 'react';
+
+export default function MessageItem({ message, currentUserId, friend }) {
+  const isSelf = message.sender_id === currentUserId;
+  const senderName = isSelf? 'me' : friend?.username;
+
+  return (
+    <div className={`message-item ${isSelf? 'self-align' : 'friend-align'}`}>
+      <span className="message-item-sender">{senderName}</span>
+      <p className="message-item-text">{message.content}</p>
+    </div>
+  );
+}
