20dc818b46951e9f4fea7ffdd7f4ba4afb053da8
djibril424 <141808900+djibril424@users.noreply.github.com>
2026-06-01 22:20:58 +0100

    Fix formatting issues in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -43,7 +43,7 @@ export default function App() {
       } else {
         setLoading(false);
       }
-    });
+    }, []);
 
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
